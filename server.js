d99b856dc8bcf55e7dbb15e68603883b800310be
rookenthusiast <rookenthusiast@gmail.com>
2017-04-05 16:58:11 +0100

    can now archive files by creating new archive folder and moving them to it and deleting the now processed files

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var fs = require('fs');
+var mv = require('mv')
 var parseString = require('xml2js').parseString;
 var DatabaseQuery = require('./db/databaseQuery')
 var MongoClient = require('mongodb').MongoClient
+
 
 var dataBase = new DatabaseQuery(); 
 // this.url = 'mongodb://localhost:27017/companyFinances';
@@ -14,7 +16,7 @@ var dataBase = new DatabaseQuery();
 var processDir = function(){
 	console.log('finding directory')
 	var totalBytes = 0;
-	var dir = '/home/cameron/Documents/xml_data';
+	var dir = '/home/cameron/Documents/xml_data/xml/';
 	fs.readdir(dir, function(err, files){
 		if (err) {
 			console.log("cant find directory") 
@@ -22,7 +24,8 @@ var processDir = function(){
 			console.log('attempting to read files')
 			var filesCompleted = 0;
 			files.forEach(function(file){
-				fs.readFile(dir + '/' + file, 'UTF-8',function(err,data){
+				fs.readFile(dir + file, 'UTF-8',function(err,data){
+					console.log(data);
 					if (err) {
 						console.log("error reading file", err);
 						} else {
@@ -44,7 +47,17 @@ var processDir = function(){
 						dataBase.add(newJsonObj, id, "JsonFiles");
 						dataBase.add(returnedDebtItem, id, 'ReturnedDebitItems');
 						filesCompleted += 1;
-						} 
+
+
+						mv(dir + file, '/home/cameron/Documents/xml_data/archive/'+ file,{mkdirp: true}, function(err){
+							if (err) {
+								console.log("been an error archiving", err);
+							}
+							else {
+								console.log("file has been archived");
+							}
+						})
+					} 
 
 						
 
@@ -54,6 +67,8 @@ var processDir = function(){
 						console.log(filesCompleted + ": file conversions completed")
 						dataBase.all("JsonFiles");
 						dataBase.all("ReturnedDebitItems");
+
+
 						}	
 				})
 			})
